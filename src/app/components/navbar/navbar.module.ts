4bf07763acd515b4c71e9359a714fe0b795cc57f
Felipe Côrtes <contatos.cortes@gmail.com>
2020-03-06 22:32:17 -0300

    New navbar style

--- src/app/components/navbar/navbar.module.ts
+++ src/app/components/navbar/navbar.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./navbar.component";
 import {SearchBarModule} from "../search-bar/search-bar.module";
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
     CommonModule,
-    SearchBarModule
+    SearchBarModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent
