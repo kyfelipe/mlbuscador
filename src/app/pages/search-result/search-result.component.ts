4c6d3df4ce8a5b433ecf04bdbe3cb1e59707f155
Felipe Côrtes <contatos.cortes@gmail.com>
2020-04-17 16:19:33 -0300

    Adicionado exibição do cep e cidade no header do resultado da busca

--- src/app/pages/search-result/search-result.component.ts
+++ src/app/pages/search-result/search-result.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { SearchResponse } from '../../shared/model/search/search-response.model';
+
 @Component({
   selector: 'mlb-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.scss']
 })
 export class SearchResultComponent implements OnInit {
-  data: any;
+  public search: SearchResponse;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.data = this.route.snapshot.data;
-    console.log(this.data);
+    this.search = this.route.snapshot.data.search;
+    console.log(this.search);
   }
-
 }
